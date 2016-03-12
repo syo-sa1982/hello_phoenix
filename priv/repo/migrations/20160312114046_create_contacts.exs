c82fd1bc1bbdcc985f25aa4c79eedb159d882f84
syo-sa1982 <fujii1308@gmail.com>
2016-03-12 21:24:07 +0900

    migrate

--- priv/repo/migrations/20160312114046_create_contacts.exs
+++ priv/repo/migrations/20160312114046_create_contacts.exs
@@ -2,5 +2,11 @@ defmodule HelloPhoenix.Repo.Migrations.CreateContacts do
   use Ecto.Migration
 
   def change do
+    create table(:contacts)do
+      add :name, :string
+      add :phone, :string
+
+      timestamps
+    end
   end
 end
